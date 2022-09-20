069c682607782b44174e12436ba15412cda5a6d6
Leyder Quintero <lele.quinter@gmail.com>
2022-09-19 20:37:08 -0500

    GifsService

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -10,8 +11,16 @@ export class BusquedaComponent {
 
   @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  buscar(){
+  constructor(
+    private gifSvc: GifsService
+  ) { }
+
+  buscar() {
+
+    this.gifSvc.buscarGifs(this.txtBuscar.nativeElement.value);
+
     this.txtBuscar.nativeElement.value = '';
+
     // const input = document.querySelector('input')!.value = '';
   }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get historial(){
+    return this.gifSvc.historial;
   }
+
+  constructor(
+    public gifSvc: GifsService
+  ) { }
+
 
 }
