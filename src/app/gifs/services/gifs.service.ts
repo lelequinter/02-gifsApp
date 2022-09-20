39195623464c53ac62c0a963f283f072186f4710
Leyder Quintero <lele.quinter@gmail.com>
2022-09-20 10:34:32 -0500

    Realizar una petición HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey: string = 'WmRLshdXp34hRaOOxDGJxrkzd81qcTDT';
   private _historial: string[] = [];
 
   get historial() {
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient){}
 
   buscarGifs(query: string = '') {
 
@@ -17,10 +21,21 @@ export class GifsService {
 
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
     }
+    // console.log(this._historial);
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=WmRLshdXp34hRaOOxDGJxrkzd81qcTDT&q=lilo&limit=10')
+    //   .then(resp => {
+    //     resp.json().then(data => console.log(data))
+    //   })
 
-    this._historial = this._historial.splice(0, 10);
-    console.log(this._historial);
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=WmRLshdXp34hRaOOxDGJxrkzd81qcTDT&q=lilo&limit=10`)
+    .subscribe( (resp: any) => {
+      console.log(resp.data);
+
+    } )
   }
 
 }
+
+
